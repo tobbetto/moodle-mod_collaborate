588f4a09a5a2ab5e464b554e24345368f703c5a9
Thorvaldur Konradsson <tobbetto@users.noreply.github.com>
2024-04-29 13:42:50 +0200

    Initial State

---
+++ lang/en/collaborate.php
@@ -0,0 +1,48 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for collaborate
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod_collaborate
+ * @copyright  2019 Richard Jones richardnz@outlook.com
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @see https://github.com/moodlehq/moodle-mod_collaborate
+ * @see https://github.com/justinhunt/moodle-mod_collaborate
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'collaborate';
+$string['modulenameplural'] = 'collaborates';
+$string['modulename_help'] = 'Use the collaborate module for... | The collaborate module allows...';
+$string['collaborate:addinstance'] = 'Add a new collaborate';
+$string['collaborate:submit'] = 'Submit collaborate';
+$string['collaborate:view'] = 'View collaborate';
+$string['collaboratefieldset'] = 'Custom example fieldset';
+$string['collaboratename'] = 'collaborate name';
+$string['collaboratename_help'] = 'This is the content of the help tooltip associated with the collaboratename field. Markdown syntax is supported.';
+$string['collaborate'] = 'collaborate';
+$string['pluginadministration'] = 'collaborate administration';
+$string['pluginname'] = 'collaborate';
+$string['nocollaborates'] = 'No instances';
+
+// Mod form specific collaborate settings.
+$string['title'] = 'Activity Title: ';
